0d935a497a6df63edb1780cf295ee566d78982a5
tonlopes21 <niltonlopes21@gmail.com>
2022-01-11 13:28:38 -0300

    atualização do bootcamp

---
+++ aula 2 - variaveis/script.js
@@ -0,0 +1,60 @@
+// //tipos primitivos
+
+// //boolean
+
+// var vOuf = false;
+// console.log(typeof(vOuf))
+
+// //number
+// var numeroQualquer = 1;
+// console.log(typeof(numeroQualquer))
+
+// //string
+// var nome = 'Nilton';
+// console.log(typeof(nome));
+
+// // function
+
+
+// var funcao = function(){}
+// console.log(typeof(funcao));
+
+// // como declarar
+// var variavel = 'Nilton';
+// varriavel = 'Lopes';
+// console.log(variavel)
+
+// //const 
+// //variável de somente leitura, não podendo ter o seu valor alterado.
+
+// // const constante = 'Nilton'
+// // constante = 'Lopes';
+
+// var escopoGlobal = 'global';
+// console.log(escopoGlobal);
+// function escopoLocal(){
+//     let escopoLocalInterno = 'local';
+//     console.log(escopoLocalInterno);
+// }
+
+
+//atribuição
+// var atribuicao = 'Nilton';
+
+// //comparação
+
+// var comparacao = 0 == 0;
+
+// //comparação identifica
+
+
+
+// var comparacaoIdentifica = '0' == 0;
+// console.log(comparacaoIdentifica);
+
+// var multiplicacao = 3 * 3
+// console.log(multiplicacao)
+
+// var subtracao = 2 - 1
+// console.log("O resultado é " + subtracao)
+
